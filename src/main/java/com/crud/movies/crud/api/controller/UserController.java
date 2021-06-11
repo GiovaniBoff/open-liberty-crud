fe45af32da5431b3ec45b904172e51291fe82931
Giovani Boff <giovanisouza15@gmail.com>
2021-06-11 11:49:35 -0300

    starting user service implementation

--- src/main/java/com/crud/movies/crud/api/controller/UserController.java
+++ src/main/java/com/crud/movies/crud/api/controller/UserController.java
@@ -4,7 +4,6 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
